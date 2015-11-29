f0240f39b496b28fb1a9e4bada46ee5ba8cd75ba
Paweł Szulc <paul.szulc@gmail.com>
2015-11-29 15:48:20 +0100

    Fix code, use compare as call in lambda

--- hand.erl
+++ hand.erl
@@ -11,7 +11,7 @@ allHands(Cards) ->
     [T || [_,_|T] <- perms(Cards)].
 
 pickBest(Cards) ->
-    nth(1,sort(compare, allHands(Cards))).
+    nth(1,sort(fun(H1,H2) -> compare(H1,H2) end, allHands(Cards))).
 
 %% thinking how to implement hands compare function...
 compare(Hand1,Hand2) ->
