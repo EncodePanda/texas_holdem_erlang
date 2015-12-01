3fd305b4e13936f8391a319cdb5e416ac073cc05
Paweł Szulc <paul.szulc@gmail.com>
2015-12-02 00:25:33 +0100

    Add (not complete) implementation of rank(Hand)

--- hand.erl
+++ hand.erl
@@ -1,6 +1,6 @@
 -module(hand).
--export([pickBest/1, compare/2]).
--import(lists, [sort/2, nth/2]).
+-export([pickBest/1, compare/2, rank/1]).
+-import(lists, [sort/2, nth/2, map/2]).
 
 
 perms([]) -> [[]];
@@ -11,9 +11,29 @@ allHands(Cards) ->
     [T || [_,_|T] <- perms(Cards)].
 
 pickBest(Cards) ->
-    nth(1,sort(fun(H1,H2) -> compare(H1,H2) end, allHands(Cards))).
+    nth(1,sort(compare/2, allHands(Cards))).
 
-%% thinking how to implement hands compare function...
 compare(Hand1,Hand2) ->
     true.
 
+figure_to_int(jack) -> 11;
+figure_to_int(queen) -> 12;
+figure_to_int(king) -> 13;
+figure_to_int(ace) -> 14;
+figure_to_int(N) -> N.
+    
+rank_card({Figure, Color}) -> {figure_to_int(Figure), Color}.
+
+arrange(RHand) ->
+    sort(fun({N1, _},{N2, _}) -> N1 >= N2 end, RHand).
+
+rank(Hand) -> 
+    rankings(arrange(map(fun rank_card/1, Hand))).
+
+rankings([{14,C}, {13,C}, {12,C}, {11,C}, {10,C}]) -> 
+    {royal_flush, C};
+rankings([{N5,C}, {N4,C}, {N3,C}, {N2,C}, {N1,C}]) 
+  when N5 == N4 + 1, N4 == N3 + 1, N3 == N2 + 1, N2 == N1 + 1 -> 
+    {flush, N5, C};
+rankings([{V, C}|_]) -> {high_card, V, C}. 
+    
